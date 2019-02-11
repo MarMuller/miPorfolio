ac58e06ebf61cbc2268b203212cb3ffc662eab53
Marina <muller.marina@hotmail.com>
2019-02-11 12:07:11 -0300

    jquery document.ready

--- js/image-effect.js
+++ js/image-effect.js
@@ -1,15 +1,19 @@
-var winWidth = $(window).width();
-
 //IMAGEN N TEXT MOVEMENT
 
-$('.banner').mousemove(function(event) {
-  var moveX = ( (winWidth / 2) - event.pageX) * 0.05;
-  var moveY = ( (winWidth / 2) - event.pageY) * 0.05;
-  $('.img-banner').css('margin-left', moveX+'px');
-  $('.img-banner').css('margin-top', moveY+'px');
-})
+$(document).ready(function(){
 
-$('.banner').mouseout(function(event) {
-  $('.img-banner').css('margin-left', 0);
-  $('.img-banner').css('margin-top', 0);
-})
+  var winWidth = $(window).width();
+
+  $('.banner').mousemove(function(event) {
+    var moveX = ( (winWidth / 2) - event.pageX) * 0.05;
+    var moveY = ( (winWidth / 2) - event.pageY) * 0.05;
+    $('.img-banner').css('margin-left', moveX+'px');
+    $('.img-banner').css('margin-top', moveY+'px');
+  })
+
+  $('.banner').mouseout(function(event) {
+    $('.img-banner').css('margin-left', 0);
+    $('.img-banner').css('margin-top', 0);
+  })
+
+});
