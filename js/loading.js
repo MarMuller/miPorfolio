b9dedc35f5ce586ec60cf5768b651c8dac3628a7
Marina <muller.marina@hotmail.com>
2019-03-26 22:32:54 -0300

    intento para orden de carga

--- js/loading.js
+++ js/loading.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
-  $('body').hide().fadeIn(1500).delay(6000)
+
+  var navButtons =  $("#nav-list > li");
+  //console.log(navButtons);
+
+  var buttonsDelay = 300;
+
+  // $(navButtons.get().reverse()).each(function (i) {
+  //   $(this).hide().delay(buttonsDelay+=200).fadeIn(1500).delay(10000);
+  //   //$(this).text( 'Item ' + (++i));
+  // });
+
+  var bannerText =  $(".txt-container");
+  $(bannerText).hide().delay(600).fadeIn(1500).delay(10000);
+
 });
