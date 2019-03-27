f4b1e39fa865b14280e371ab04d4cfe99825f18e
Marina <muller.marina@hotmail.com>
2019-03-27 15:37:32 -0300

    por ahora todo sin bugs

--- index.php
+++ index.php
@@ -14,6 +14,90 @@
     <!-- CONTENEDOR PRINCIPAL -->
     <?php include('includes/main-banner.php');?>
 
+    <!-- GALERIA -->
+    <h3 class="centerText">#Ilustraciones</h3>
+
+    <div class="myGallery">
+
+      <div class="previewGallery">
+        Holaaaaa
+      </div>
+      <div class="previewGallery">
+        Holaaaaa
+      </div>
+      <div class="previewGallery">
+        Holaaaaa
+      </div>
+      <div class="previewGallery">
+        Holaaaaa
+      </div>
+      <div class="previewGallery">
+        Holaaaaa
+      </div>
+      <div class="previewGallery">
+        Holaaaaa
+      </div>
+
+      <div class="previewGallery">
+        Holaaaaa
+      </div>
+      <div class="previewGallery">
+        Holaaaaa
+      </div>
+      <div class="previewGallery">
+        Holaaaaa
+      </div>
+      <div class="previewGallery">
+        Holaaaaa
+      </div>
+      <div class="previewGallery">
+        Holaaaaa
+      </div>
+      <div class="previewGallery">
+        Holaaaaa
+      </div>
+
+      <div class="previewGallery">
+        Holaaaaa
+      </div>
+      <div class="previewGallery">
+        Holaaaaa
+      </div>
+      <div class="previewGallery">
+        Holaaaaa
+      </div>
+      <div class="previewGallery">
+        Holaaaaa
+      </div>
+      <div class="previewGallery">
+        Holaaaaa
+      </div>
+      <div class="previewGallery">
+        Holaaaaa
+      </div>
+
+      <div class="previewGallery">
+        Holaaaaa
+      </div>
+      <div class="previewGallery">
+        Holaaaaa
+      </div>
+      <div class="previewGallery">
+        Holaaaaa
+      </div>
+      <div class="previewGallery">
+        Holaaaaa
+      </div>
+      <div class="previewGallery">
+        Holaaaaa
+      </div>
+      <div class="previewGallery">
+        Holaaaaa
+      </div>
+
+    </div>
+    <!-- FIN GALERIA -->
+
   </body>
 
 </html>
