30f308ba5c14c611e4172575cc9a97f3c6073d55
Marina <muller.marina@hotmail.com>
2019-03-29 00:20:02 -0300

    otra cosita

---
+++ includes/gallery.php
@@ -0,0 +1,94 @@
+<div class="myGallery">
+
+  <ul>
+    <li class="previewGallery">
+      Holaaaaa
+    </li>
+    <li class="previewGallery">
+      Holaaaaa
+    </li>
+    <li class="previewGallery">
+      Holaaaaa
+    </li>
+    <li class="previewGallery">
+      Holaaaaa
+    </li>
+    <li class="previewGallery">
+      Holaaaaa
+    </li>
+    <li class="previewGallery">
+      Holaaaaa
+    </li>
+
+    <li class="previewGallery">
+      Holaaaaa
+    </li>
+    <li class="previewGallery">
+      Holaaaaa
+    </li>
+    <li class="previewGallery">
+      Holaaaaa
+    </li>
+    <li class="previewGallery">
+      Holaaaaa
+    </li>
+    <li class="previewGallery">
+      Holaaaaa
+    </li>
+    <li class="previewGallery">
+      Holaaaaa
+    </li>
+
+    <li class="previewGallery">
+      Holaaaaa
+    </li>
+    <li class="previewGallery">
+      Holaaaaa
+    </li>
+    <li class="previewGallery">
+      Holaaaaa
+    </li>
+    <li class="previewGallery">
+      Holaaaaa
+    </li>
+    <li class="previewGallery">
+      Holaaaaa
+    </li>
+    <li class="previewGallery">
+      Holaaaaa
+    </li>
+
+    <li class="previewGallery">
+      Holaaaaa
+    </li>
+    <li class="previewGallery">
+      Holaaaaa
+    </li>
+    <li class="previewGallery">
+      Holaaaaa
+    </li>
+    <li class="previewGallery">
+      Holaaaaa
+    </li>
+    <li class="previewGallery">
+      Holaaaaa
+    </li>
+    <li class="previewGallery">
+      Holaaaaa
+    </li>
+
+    <!-- NO BORRAR -->
+    <li class="previewGallery"></li>
+    <li class="previewGallery"></li>
+    <li class="previewGallery"></li>
+    <li class="previewGallery"></li>
+    <li class="previewGallery"></li>
+    <li class="previewGallery"></li>
+    <li class="previewGallery"></li>
+    <li class="previewGallery"></li>
+    <li class="previewGallery"></li>
+    <li class="previewGallery"></li>
+
+  </ul>
+
+</div>
